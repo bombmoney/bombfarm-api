376b01c208474ab461a3a07e8826ba9839f50d3a
Aaron Shames <aaron@1coin.io>
2022-07-25 15:26:01 -0400

    fixed buyback data

--- src/api/stats/bifibuyback/etherscanApiUrlMap.ts
+++ src/api/stats/bifibuyback/etherscanApiUrlMap.ts
@@ -4,30 +4,30 @@
 export const etherscanApiUrlMap = {
   bsc: {
     url: 'https://api.bscscan.com',
-    apiToken: '1R1VIWFIS3B3SZRVD5W9EV68SRCNB8ZH77',
+    apiToken: 'YWA6YXGBRV3279EMVUPICMWR5TZC1K4UUC',
   },
-  polygon: {
-    url: 'https://api.polygonscan.com',
-    apiToken: 'AXIPYQNDK2M98DMKSRMZEJ78PIJSBA9K3V',
-  },
-  fantom: {
-    url: 'https://api.ftmscan.com',
-    apiToken: 'HT3241A1K3ZVN4368JGHAFJXRR36VD8NCC',
-  },
-  heco: {
-    url: 'https://api.hecoinfo.com',
-    apiToken: '6XRRGTTB5ZQWXV55IDQJWYNF2F4GK7I1KK',
-  },
-  avax: {
-    url: 'https://api.snowtrace.io',
-    apiToken: 'RAQVQ1H9766QWV3JYF49CCYCB6WAK8STVB',
-  },
-  moonriver: {
-    url: 'https://api-moonriver.moonscan.io',
-    apiToken: 'ERD8CW11NEI89IFQPJ1I5SJTCIDEG9SZCW',
-  },
-  arbitrum: {
-    url: 'https://api.arbiscan.io',
-    apiToken: 'QR9KETWZEU64T1TQN4V8VD7H6HIE3X95UW',
-  },
+  // polygon: {
+  //   url: 'https://api.polygonscan.com',
+  //   apiToken: 'AXIPYQNDK2M98DMKSRMZEJ78PIJSBA9K3V',
+  // },
+  // fantom: {
+  //   url: 'https://api.ftmscan.com',
+  //   apiToken: 'HT3241A1K3ZVN4368JGHAFJXRR36VD8NCC',
+  // },
+  // heco: {
+  //   url: 'https://api.hecoinfo.com',
+  //   apiToken: '6XRRGTTB5ZQWXV55IDQJWYNF2F4GK7I1KK',
+  // },
+  // avax: {
+  //   url: 'https://api.snowtrace.io',
+  //   apiToken: 'RAQVQ1H9766QWV3JYF49CCYCB6WAK8STVB',
+  // },
+  // moonriver: {
+  //   url: 'https://api-moonriver.moonscan.io',
+  //   apiToken: 'ERD8CW11NEI89IFQPJ1I5SJTCIDEG9SZCW',
+  // },
+  // arbitrum: {
+  //   url: 'https://api.arbiscan.io',
+  //   apiToken: 'QR9KETWZEU64T1TQN4V8VD7H6HIE3X95UW',
+  // },
 } as const;
